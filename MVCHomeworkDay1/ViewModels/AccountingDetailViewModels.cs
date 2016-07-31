2490022a417b32a3bde221556068a61deea74938
RojerChen <rojerchen@hotmail.com>
2016-07-31 14:55:11 +0800

    新增 AccountingDetailViewModels

---
+++ MVCHomeworkDay1/ViewModels/AccountingDetailViewModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeworkDay1.ViewModels
+{
+    public class AccountingDetailViewModels
+    {
+        //收入、支出
+        public string Type { get; set; }
+
+        //日期
+        public string Date { get; set; }
+
+        //金額總計
+        public decimal Sum { get; set; }
+
+        //備註
+        public string Note { get; set; }
+    }
+}
