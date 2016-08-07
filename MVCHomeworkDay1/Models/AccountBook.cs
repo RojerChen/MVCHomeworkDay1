9790e60d82abb0087236868fb10becb10698440b
RojerChen <rojerchen@hotmail.com>
2016-08-07 11:23:38 +0800

    add Model AccountBook

---
+++ MVCHomeworkDay1/Models/AccountBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeworkDay1.Models
+{
+    public class AccountBook
+
+    {
+        public Guid Id { get; set; }
+
+        public int Categoryyy { get; set; }
+
+        public int Amounttt { get; set; }
+
+        public DateTime Dateee { get; set; }
+
+        public string Remarkkk { get; set; }
+    }
+}
